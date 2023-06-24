0e7e1c76fe698b07d4752dfe710f508a0affc0ef
haf9800 <hafsaalgawda@gmail.com>
2023-06-24 14:50:39 +0400

    Initial Commit

---
+++ cookies/ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  cookies
+//
+//  Created by hafsa ameen  on 24/06/2023.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var counter = 0
+    @State var message = ""
+    var body: some View {
+        VStack {
+            Text("\(counter) cookies eaten")
+                .font(.title)
+            Button{
+                self.counter += 1
+                if counter >= 40{
+                    message = "Oh Nooooooooo 😭"
+                }
+                
+                else if counter >= 30 {
+                    message = "What are you doing??? 😰"
+                }
+                
+                else if counter >= 20 {
+                    message = "Don't eat too much cookies☹️"
+                }
+                
+            }label:{
+                Text("OM NOM NOM")
+            }
+            .padding()
+            .background(.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            .shadow(color: .black, radius: 5, x: 0.0, y: 0.0 )
+          
+        
+            }
+            Text(message)
+            .padding()
+        
+        }
+    
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cookies/cookiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  cookiesApp.swift
+//  cookies
+//
+//  Created by hafsa ameen  on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct cookiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
